c3904be6c6b8c6735c542079ca2dcde93cf6e17a
dangxuanthangqt <dangxuanthangqt@gmail.com>
2021-06-23 06:03:17 +0700

    custom

--- src/client.js
+++ src/client.js
@@ -2,6 +2,7 @@ import { ApolloClient } from '@apollo/client';
 import cache from './graphql/cache';
 
 const client = new ApolloClient({
+  uri:'http://localhost:8080/graphql',
   cache,
   connectToDevTools: true,
 });
--- src/pages/landingPage.jsx
+++ src/pages/landingPage.jsx
@@ -1,15 +1,29 @@
 import React from 'react'
-import { useQuery } from '@apollo/client';
+import { gql, useQuery } from '@apollo/client';
 import ButtonComponent from '../components/button';
 import { GET_DARK_MODE } from '../graphql/reactivities/theme';
 
 const LandingPage = () => {
 
-  const {loading, error, data} = useQuery(GET_DARK_MODE);
-
+  const { loading, error, data } = useQuery(GET_DARK_MODE);
+  const { loading: l, error: e, data: d } = useQuery(gql`
+    query {
+      getAllFriend {
+        lastName
+        firstName
+      }
+    }
+  `, {
+    //pollInterval: 500,
+    onCompleted: (data) => console.log(data),
+    notifyOnNetworkStatusChange: true
+  })
   return (
     <div style={{ height: '100vh', backgroundColor: data.darkMode ? 'black' : 'white', color: data.darkMode ? 'white' : 'black' }}>
-      <h1>Welcome to Theme Toggle Appliation!</h1>
+      <h1> Welcome to Theme Toggle Appliation!</h1>
+      {
+        d?.getAllFriend.map((item) => <p key={item.id}>{item.lastName}</p>)
+      }
       <ButtonComponent />
     </div>
   )
